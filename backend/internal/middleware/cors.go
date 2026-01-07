cbf4664213bf4f2a8cdc1ad2a222a2b41d9327d2
Lê Duy Khang <lehoanggiakhuong2006@gmail.com>
2026-01-08 03:20:33 +0700

    Update cors.go

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "https://high-contention-resource-allocation.vercel.app/simulations/voucher-allocation")
+		c.Header("Access-Control-Allow-Origin", "https://high-contention-resource-allocation.vercel.app")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type")
 
